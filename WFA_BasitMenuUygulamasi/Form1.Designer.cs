3d469c4071af35cd1e66da0a4828730c7b7e4a70
eylem <eylem@EYLEM>
2024-12-23 18:50:55 +0300

    Add project files.

---
+++ WFA_BasitMenuUygulamasi/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace WFA_BasitMenuUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1_hesapla = new System.Windows.Forms.Button();
+            this.checkBox1_cay = new System.Windows.Forms.CheckBox();
+            this.checkBox2_Kahve = new System.Windows.Forms.CheckBox();
+            this.checkBox3_milkshake = new System.Windows.Forms.CheckBox();
+            this.checkBox4_kola = new System.Windows.Forms.CheckBox();
+            this.checkBox5_fanta = new System.Windows.Forms.CheckBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox6_gazoz = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2_tutar = new System.Windows.Forms.Label();
+            this.button2_TEMİZLE = new System.Windows.Forms.Button();
+            this.button3_ONAYLA = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1_hesapla
+            // 
+            this.button1_hesapla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1_hesapla.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1_hesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1_hesapla.ForeColor = System.Drawing.Color.Black;
+            this.button1_hesapla.Location = new System.Drawing.Point(41, 474);
+            this.button1_hesapla.Name = "button1_hesapla";
+            this.button1_hesapla.Size = new System.Drawing.Size(116, 47);
+            this.button1_hesapla.TabIndex = 0;
+            this.button1_hesapla.Text = "HESAPLA";
+            this.button1_hesapla.UseVisualStyleBackColor = false;
+            this.button1_hesapla.Click += new System.EventHandler(this.button1_hesapla_Click);
+            // 
+            // checkBox1_cay
+            // 
+            this.checkBox1_cay.AutoSize = true;
+            this.checkBox1_cay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1_cay.Location = new System.Drawing.Point(41, 42);
+            this.checkBox1_cay.Name = "checkBox1_cay";
+            this.checkBox1_cay.Size = new System.Drawing.Size(97, 24);
+            this.checkBox1_cay.TabIndex = 1;
+            this.checkBox1_cay.Text = "Çay(25)";
+            this.checkBox1_cay.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2_Kahve
+            // 
+            this.checkBox2_Kahve.AutoSize = true;
+            this.checkBox2_Kahve.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox2_Kahve.Location = new System.Drawing.Point(197, 42);
+            this.checkBox2_Kahve.Name = "checkBox2_Kahve";
+            this.checkBox2_Kahve.Size = new System.Drawing.Size(116, 24);
+            this.checkBox2_Kahve.TabIndex = 2;
+            this.checkBox2_Kahve.Text = "Kahve(80)";
+            this.checkBox2_Kahve.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3_milkshake
+            // 
+            this.checkBox3_milkshake.AutoSize = true;
+            this.checkBox3_milkshake.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox3_milkshake.Location = new System.Drawing.Point(352, 42);
+            this.checkBox3_milkshake.Name = "checkBox3_milkshake";
+            this.checkBox3_milkshake.Size = new System.Drawing.Size(158, 24);
+            this.checkBox3_milkshake.TabIndex = 3;
+            this.checkBox3_milkshake.Text = "Milkshake(100)";
+            this.checkBox3_milkshake.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4_kola
+            // 
+            this.checkBox4_kola.AutoSize = true;
+            this.checkBox4_kola.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox4_kola.Location = new System.Drawing.Point(41, 279);
+            this.checkBox4_kola.Name = "checkBox4_kola";
+            this.checkBox4_kola.Size = new System.Drawing.Size(102, 24);
+            this.checkBox4_kola.TabIndex = 4;
+            this.checkBox4_kola.Text = "Kola(55)";
+            this.checkBox4_kola.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5_fanta
+            // 
+            this.checkBox5_fanta.AutoSize = true;
+            this.checkBox5_fanta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox5_fanta.Location = new System.Drawing.Point(197, 279);
+            this.checkBox5_fanta.Name = "checkBox5_fanta";
+            this.checkBox5_fanta.Size = new System.Drawing.Size(112, 24);
+            this.checkBox5_fanta.TabIndex = 5;
+            this.checkBox5_fanta.Text = "Fanta(50)";
+            this.checkBox5_fanta.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::WFA_BasitMenuUygulamasi.Properties.Resources._2405447;
+            this.pictureBox6.Location = new System.Drawing.Point(41, 327);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(123, 119);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 12;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::WFA_BasitMenuUygulamasi.Properties.Resources.kk;
+            this.pictureBox5.Location = new System.Drawing.Point(197, 98);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(123, 119);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WFA_BasitMenuUygulamasi.Properties.Resources.mlk;
+            this.pictureBox4.Location = new System.Drawing.Point(352, 98);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(123, 119);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WFA_BasitMenuUygulamasi.Properties.Resources._4329542;
+            this.pictureBox3.Location = new System.Drawing.Point(352, 327);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(123, 119);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WFA_BasitMenuUygulamasi.Properties.Resources.cay;
+            this.pictureBox2.Location = new System.Drawing.Point(41, 98);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 119);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WFA_BasitMenuUygulamasi.Properties.Resources.fanta_merinda;
+            this.pictureBox1.Location = new System.Drawing.Point(197, 327);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox6_gazoz
+            // 
+            this.checkBox6_gazoz.AutoSize = true;
+            this.checkBox6_gazoz.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox6_gazoz.Location = new System.Drawing.Point(352, 279);
+            this.checkBox6_gazoz.Name = "checkBox6_gazoz";
+            this.checkBox6_gazoz.Size = new System.Drawing.Size(119, 24);
+            this.checkBox6_gazoz.TabIndex = 13;
+            this.checkBox6_gazoz.Text = "Gazoz(40)";
+            this.checkBox6_gazoz.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(347, 522);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 25);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "TUTAR : ";
+            // 
+            // label2_tutar
+            // 
+            this.label2_tutar.AutoSize = true;
+            this.label2_tutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2_tutar.Location = new System.Drawing.Point(441, 522);
+            this.label2_tutar.Name = "label2_tutar";
+            this.label2_tutar.Size = new System.Drawing.Size(30, 25);
+            this.label2_tutar.TabIndex = 14;
+            this.label2_tutar.Text = "...";
+            // 
+            // button2_TEMİZLE
+            // 
+            this.button2_TEMİZLE.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button2_TEMİZLE.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2_TEMİZLE.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2_TEMİZLE.ForeColor = System.Drawing.Color.Black;
+            this.button2_TEMİZLE.Location = new System.Drawing.Point(41, 530);
+            this.button2_TEMİZLE.Name = "button2_TEMİZLE";
+            this.button2_TEMİZLE.Size = new System.Drawing.Size(116, 47);
+            this.button2_TEMİZLE.TabIndex = 0;
+            this.button2_TEMİZLE.Text = "TEMİZLE";
+            this.button2_TEMİZLE.UseVisualStyleBackColor = false;
+            this.button2_TEMİZLE.Click += new System.EventHandler(this.button2_TEMİZLE_Click);
+            // 
+            // button3_ONAYLA
+            // 
+            this.button3_ONAYLA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button3_ONAYLA.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3_ONAYLA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3_ONAYLA.Location = new System.Drawing.Point(193, 500);
+            this.button3_ONAYLA.Name = "button3_ONAYLA";
+            this.button3_ONAYLA.Size = new System.Drawing.Size(116, 47);
+            this.button3_ONAYLA.TabIndex = 15;
+            this.button3_ONAYLA.Text = "SİPARİŞİ ONAYLA";
+            this.button3_ONAYLA.UseVisualStyleBackColor = false;
+            this.button3_ONAYLA.Click += new System.EventHandler(this.button3_ONAYLA_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(563, 589);
+            this.Controls.Add(this.button3_ONAYLA);
+            this.Controls.Add(this.label2_tutar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox6_gazoz);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox5_fanta);
+            this.Controls.Add(this.checkBox4_kola);
+            this.Controls.Add(this.checkBox3_milkshake);
+            this.Controls.Add(this.checkBox2_Kahve);
+            this.Controls.Add(this.checkBox1_cay);
+            this.Controls.Add(this.button2_TEMİZLE);
+            this.Controls.Add(this.button1_hesapla);
+            this.Name = "Form1";
+            this.Text = "Menü Uygulaması";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1_hesapla;
+        private System.Windows.Forms.CheckBox checkBox1_cay;
+        private System.Windows.Forms.CheckBox checkBox2_Kahve;
+        private System.Windows.Forms.CheckBox checkBox3_milkshake;
+        private System.Windows.Forms.CheckBox checkBox4_kola;
+        private System.Windows.Forms.CheckBox checkBox5_fanta;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.CheckBox checkBox6_gazoz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2_tutar;
+        private System.Windows.Forms.Button button2_TEMİZLE;
+        private System.Windows.Forms.Button button3_ONAYLA;
+    }
+}
+
---
+++ WFA_BasitMenuUygulamasi/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_BasitMenuUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_hesapla_Click(object sender, EventArgs e)
+        {
+            int tutar = 0;
+            if (checkBox1_cay.Checked)
+            {
+                tutar += 25;
+            }
+            if (checkBox2_Kahve.Checked)
+            {
+                tutar += 80;
+            }
+            if (checkBox3_milkshake.Checked)
+            {
+                tutar += 100;
+            }
+            if (checkBox4_kola.Checked)
+            {
+                tutar += 55;
+            }
+            if (checkBox5_fanta.Checked)
+            {
+                tutar += 50;
+            }
+            if (checkBox6_gazoz.Checked)
+            {
+                tutar += 40;
+            }
+
+            label2_tutar.Text = tutar.ToString();
+        }
+
+        private void button2_TEMİZLE_Click(object sender, EventArgs e)
+        {
+            checkBox1_cay.Checked = false;
+            checkBox2_Kahve.Checked = false;
+            checkBox3_milkshake.Checked = false;
+            checkBox4_kola.Checked = false;
+            checkBox5_fanta.Checked = false;
+            checkBox6_gazoz.Checked = false;
+            label2_tutar.Text = "";
+        }
+
+        private void button3_ONAYLA_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Siparişiniz onaylanmıştır.", "Bilgi", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+    }
+}
